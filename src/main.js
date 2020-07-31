03b549dfdf3fa3a1566d77f4959125ec0982508c
Furqan Mohsin <furqanmohsin1@gmail.com>
2020-07-31 11:44:32 +0500

    Initialized app with Bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -2,11 +2,28 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import jQuery from 'jquery'
+import { BootstrapVue, IconsPlugin, ToastPlugin, ModalPlugin } from 'bootstrap-vue'
+import 'bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+global.jQuery = jQuery
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+Vue.use(ToastPlugin)
+Vue.use(ModalPlugin)
 
 new Vue({
   router,
   store,
+  beforeCreate () {
+    this.$store.commit('initialiseStore')
+  },
   render: h => h(App)
 }).$mount('#app')
